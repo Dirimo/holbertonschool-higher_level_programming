d5daccb173463dbcca0e8a4c569d6aa297c75c33
Dirimo <dirimoiriarte@gmail.com>
2025-05-09 11:26:14 +0200

    Correction of my task 2

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -7,11 +7,10 @@ if __name__ == "__main__":
 
     count = len(sys.argv) - 1
     if count == 0:
-        print("Number of argument(s): 0.")
+        print("0 arguments.")
     elif count == 1:
-        print("Number of argument(s): 1 argument:")
+        print("1 arguments:")
     else:
-        print("Number of argument(s):{} arguments:".format(count))
-
+        print("{} arguments:".format(count))
     for i in range(count):
         print("{}: {}".format(i + 1, sys.argv[i + 1]))
