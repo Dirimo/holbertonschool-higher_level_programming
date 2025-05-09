decedf2b1e7042eb625e2b8e82dcde336a4ffb16
Dirimo <dirimoiriarte@gmail.com>
2025-05-09 11:16:02 +0200

    Correction of my task 2

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -2,17 +2,17 @@
 import sys
 
 if __name__ == "__main__":
-    argv = sys.argv
-    num_args = len(argv) - 1
+    """Print the number of the list of arguments."""
+    import sys
 
+    count = len(sys.argv) - 1
+    if count == 0:
+        print("Number of argument(s): 0.")
+    elif count == 1:
+        print("Number of argument(s): 1 argument:")
+    else:
+        print("Number of argument(s):{} arguments:".format(count))
 
-if num_args == 0:
-    print("Number of argument(s): 0.")
-elif num_args == 1:
-    print("Number of argument(s): 1 argument:")
-else:
-    print("Number of argument(s):{} arguments:".format(num_args))
-
-for i in range(1, len(argv)):
-    print("{}: {}".format(i, argv[i]))
+    for i in range(count):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
 
