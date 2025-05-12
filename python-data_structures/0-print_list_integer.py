9652dd03d6b37912ee09fdf43e17599eb5e1a38d
Dirimo <dirimoiriarte@gmail.com>
2025-05-12 16:34:40 +0200

    Adding new projet

---
+++ python-data_structures/0-print_list_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def print_list_integer(my_list=[]):
+    """Prints all integers of a list.
+
+    Args:
+        my_list: A list of integers.
+    """
+    for num in my_list:
+        print("{:d}".format(num))
---
+++ python-data_structures/README.md
@@ -0,0 +1,1 @@
+new projet about data structures
