0493223be08c986625e3227f438bc27253b30428
Dirimo <dirimoiriarte@gmail.com>
2025-05-15 12:12:44 +0200

    correction of last task

--- python-more_data_structures/7-update_dictionary.py
+++ python-more_data_structures/7-update_dictionary.py
@@ -1,10 +1,5 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    """Replaces or adds key/value in a dictionary.
-
-    Args:
-        a_dictionary: The dictionary to update.
-        key: The key to add or update (string).
-        value: The value to associate with the key (any type).
-    """
+    """Replace or add key/value pairs in a dictionary."""
     a_dictionary[key] = value
+    return (a_dictionary)
