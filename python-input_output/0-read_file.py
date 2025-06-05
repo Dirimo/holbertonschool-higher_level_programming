8e7987e55364dc39fea6d7f573907db32dd9fda2
Dirimo <dirimoiriarte@gmail.com>
2025-06-05 15:05:50 +0200

    Debugging task 0

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -14,7 +14,8 @@ def read_file(filename=""):
 
 
     Args:
-        filename (str): The nae of the file to read.
+        filename (str): The name of the file to read. Defaults
+        to an empty string.
     """
     with open(filename, "r", enconding="utf-8") as file:
         read_data = file.read()    
