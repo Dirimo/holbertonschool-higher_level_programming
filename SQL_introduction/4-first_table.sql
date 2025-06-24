f8978bd8693964fff1d0898e0438a126df9490fa
Dirimo <dirimoiriarte@gmail.com>
2025-06-24 16:52:37 +0200

    Adding my readme and some tasks

---
+++ SQL_introduction/4-first_table.sql
@@ -0,0 +1,8 @@
+-- creates a table called first_table in the current database in MySQL server
+-- first_table description: id INT, name VARCHAR(256)
+-- The database name will be passed as an argument of the mysql command
+
+CREATE TABLE IF NOT EXISTS first_table (
+	int INT,
+	name VARCHAR(256)
+);
